2dcb47b9634424b15392721041d8eea148ca255b
Anton Milov <avm@gldva.ru>
2022-11-21 11:13:34 +0700

    Realisation

--- src/com/org/homework/application/Main.java
+++ src/com/org/homework/application/Main.java
@@ -1,2 +1,51 @@
-package com.org.homework.application;public class Main {
+package com.org.homework.application;
+
+import com.org.homework.customqueue.CustomQueue;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            CustomQueue<String> queue = new CustomQueue<>(7);
+
+            String[] stringArray = {"First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh"};
+
+            enQueueProcessing(queue);
+
+            for (int i = 0; i < 7; i++) {
+                queue.enqueue(stringArray[i]);
+                enQueueProcessing(queue);
+            }
+
+            //queue.enqueue("Eighth"); //Exception
+
+            for (int i = 0; i < 7; i++) {
+                deqQueueProcessing(queue);
+            }
+
+            //queue.dequeue(); //Exception
+
+        } catch (IndexOutOfBoundsException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+    }
+
+    @SuppressWarnings("rawtypes")
+    public static void enQueueProcessing(CustomQueue queue) {
+        System.out.println("Queue is empty: " + queue.isEmpty());
+        System.out.println("Queue is full: " + queue.isFull());
+        System.out.println("Queue's size: " + queue.size());
+        System.out.println(queue);
+    }
+
+    @SuppressWarnings("rawtypes")
+    public static void deqQueueProcessing(CustomQueue queue) {
+        System.out.println(queue.dequeue());
+        System.out.println("Queue is empty: " + queue.isEmpty());
+        System.out.println("Queue is full: " + queue.isFull());
+        System.out.println("Queue's size: " + queue.size());
+        System.out.println(queue);
+    }
+
 }
--- src/com/org/homework/customqueue/CustomQueue.java
+++ src/com/org/homework/customqueue/CustomQueue.java
@@ -1,2 +1,67 @@
-package com.org.homework.customqueue;public class CustomQueue {
+package com.org.homework.customqueue;
+
+
+import java.util.Arrays;
+
+public class CustomQueue<T> {
+
+    private final T[] array;
+    private int front;
+    private int rear;
+    private final int capacity;
+    private int count;
+
+    @Override
+    public String toString() {
+        return "CustomQueue{" +
+                "array=" + Arrays.toString(array) +
+                ", front=" + front +
+                ", rear=" + rear +
+                ", capacity=" + capacity +
+                ", count=" + count +
+                '}';
+    }
+
+    @SuppressWarnings("unchecked")
+    public CustomQueue(int size) {
+        this.array = (T[]) new Object[size];
+        this.rear = size - 1;
+        this.capacity = size;
+        this.count = 0;
+    }
+
+    public boolean isEmpty() {
+        return (count == 0);
+    }
+
+    public boolean isFull() {
+        return (count == capacity);
+    }
+
+    public void enqueue(T element) {
+        if (!isFull()) {
+            rear = (rear + 1) % array.length;
+            array[rear] = element;
+            count++;
+        } else {
+            throw new IndexOutOfBoundsException("Queue if full");
+        }
+    }
+
+    public T dequeue() {
+        if (!isEmpty()) {
+            T deqValue = array[front];
+            array[front] = null;
+            front = (front + 1) % array.length;
+            count--;
+            return deqValue;
+        } else {
+            throw new IndexOutOfBoundsException("Queue is empty");
+        }
+    }
+
+    public int size() {
+        return count;
+    }
+
 }
